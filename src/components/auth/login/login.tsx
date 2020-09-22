bcb871a02b3b5839fab30de155354ccb23b4161f
Даниил Кочкин <reilgelern@mail.ru>
2020-09-22 22:40:40 +0300

    Установил недостающие зависимости

---
+++ src/components/auth/login/login.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './login.scss';
+
+const Login = () => {
+  return <h1>Войти</h1>
+};
+
+export default Login;
---
+++ src/components/auth/register/register.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './register.scss';
+
+const Register = () => {
+  return <h1>Регистрация</h1>
+};
+
+export default Register;
