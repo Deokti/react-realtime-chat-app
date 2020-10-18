3d41252e9a5787c20eacc6d35b69de8d70c193df
Deokti <reilgelern@mail.ru>
2020-10-18 19:37:11 +0300

    Добавил спиннер и некоторые другие элементы.

--- src/components/spinner/spinner.tsx
+++ src/components/spinner/spinner.tsx
@@ -3,9 +3,9 @@ import React from "react";
 import './spinner.scss';
 import { SpinnerLoader } from "../icon";
 
-const Spinner = () => {
+const Spinner = ({ position = 'fixed' }: any) => {
   return (
-    <div className="spinner">
+    <div className="spinner" style={{ position: position }}>
       <SpinnerLoader width={100} height={100} />
       <span className="spinner-text">Подождите, идёт загрузка...</span>
     </div>
