5ae5c772eaa4af6abc588e249ba2b7a926150e2f
Deokti <reilgelern@mail.ru>
2020-10-30 14:33:55 +0300

    Изменил расположение компомента модального окна для отображения предварительного показа. Теперь он занимает весь экран и убрать его можно только в двух случаях, если нажать на Отмена или Отправить. В остальном убрать модальное окно невозможно.

--- src/components/message-panel/message-panel-modal/message-panel-modal.tsx
+++ src/components/message-panel/message-panel-modal/message-panel-modal.tsx
@@ -17,19 +17,6 @@ const MessagePanelModal: React.FC<TMessagePanelModal> = ({ previewImage, current
 
   const closeModal = useCallback(() => currentSelectedImage(null), [ currentSelectedImage ]);
 
-  // Если произошёл клик не по элементу, то активируется функция и закроет окно
-  const onClickNotMessageModal = useCallback((event: any) => {
-    if (event.path && !event.path.includes(messageModalRef.current)) {
-      closeModal();
-    }
-  }, [ closeModal ]);
-
-  useEffect(() => {
-    document.body.addEventListener('click', onClickNotMessageModal);
-    return () => document.body.removeEventListener('click', onClickNotMessageModal);
-  }, [ onClickNotMessageModal ]);
-
-
   return (
     <div className="message-panel-modal" ref={messageModalRef}>
       <div className="message-panel-modal__wrapper">
